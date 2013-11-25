df1f1222b47478dcb324b8fff7a536bf07daebb8
Fabien Proriol <fabien@kazoe.org>
2013-11-25 15:53:19 +0100

    Add Youtube plugin

--- lib/tinymce_config.php
+++ lib/tinymce_config.php
@@ -12,19 +12,20 @@ tinymce.init({
 	plugins: [
 		"advlist autolink link image lists charmap print preview hr anchor pagebreak",
 		"wordcount visualblocks visualchars code fullscreen insertdatetime nonbreaking",
-		"table contextmenu directionality paste textcolor responsivefilemanager"
+		"table contextmenu directionality paste textcolor responsivefilemanager youtube"
 	],
 	image_advtab: false,
 	extended_valid_elements: "table[class=table|border:0|width=100%]",
 	filemanager_title:"Choisir le fichier" ,
 	external_filemanager_path:"/kazoe/lib/filemanager/",
 	external_plugins: { "filemanager" : "../filemanager/plugin.min.js"},
+	external_plugins: { "youtube" : "../youtube/plugin.min.js"},
 <?php
 	if(is_file($Kz->getPath('docroot').'/skin/'.$Kz->getEnv('SKIN')."/css/editor.css"))
 	{
 		echo "\tcontent_css : \"/skin/".$Kz->getEnv('SKIN')."/css/editor.css\",\n";
 	}
 ?>
-	toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table tablecontrols | link unlink anchor image | fullpage <?php if($Kz->getEnv('USER_ID') == 0) echo 'code'; ?>"
+	toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table tablecontrols | link unlink anchor image youtube | fullpage <?php if($Kz->getEnv('USER_ID') == 0) echo 'code'; ?>"
 	
 });
